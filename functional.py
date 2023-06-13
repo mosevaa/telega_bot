c7404ce5166f04141d25d21c928815e38c497ef6
Alesya Moseva <mosevaa13@gmail.com>
2023-06-13 21:04:19 +0300

    done

--- functional.py
+++ functional.py
@@ -1,35 +1,15 @@
-import csv
-import time
-import logging
-
-from typing import List
-from pyrogram import Client
+import asyncio
+from pyrogram.errors import BadRequest, FloodWait
 
 
-api_id = 0
-api_hash = ""
-
-
-def parse_cvs(filepath: str) -> List[str]:
-    with open(filepath, newline='') as csvfile:
-        urls_reader = csv.reader(csvfile, delimiter='\n')
-        urls = []
-        for url in urls_reader:
-            urls.append(url[0])
-        return urls
-
-
-async def join_chats():
-    filepath = '.\\urls.csv'
-    app = Client('account', api_id=api_id, api_hash=api_hash)
-    urls = parse_cvs(filepath)
-    i = 0
-
-    logging.info('Начинаю вступать в группы')
-
+async def join(urls: list[str], app):
     for url in urls:
-        i += 1
-        await app.join_chat(url)
-        if i == 5:
-            logging.info('Вступил в 5 групп, ожидаю 5 минут, чтобы продолжить')
-            time.sleep(301)
+        try:
+            print(url)
+            await app.join_chat(url)
+        except BadRequest as e:
+            pass
+        except FloodWait as e:
+            print(f'I wait {e.value}')
+            await asyncio.sleep(e.value)
+
--- routers.py
+++ routers.py
@@ -1,13 +1,26 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, Message
-from aiogram.filters import Command
+from aiogram.filters import Command, Text
 from aiogram.fsm.context import FSMContext
 
 from pyrogram import Client
+from pyrogram.types import User
 
+from functional import join
 
 router = Router()
+
+
+def make_row_keyboard(items: list[str]) -> ReplyKeyboardMarkup:
+
+    row = [KeyboardButton(text=item) for item in items]
+    return ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True, one_time_keyboard=True)
+
+
+available_answers = ['Да', 'Нет']
+start_button = ['Начать']
+return_button = ['Назад']
 
 
 class Order(StatesGroup):
@@ -15,19 +28,44 @@ class Order(StatesGroup):
     api_hash = State()
     number = State()
     code = State()
+    auth = State()
+    urls_list = State()
 
 
-def make_keyboard(items: list[str]) -> ReplyKeyboardMarkup:
-    row = [KeyboardButton(text=item) for item in items]
-    return ReplyKeyboardMarkup(keyboard=[row], resize_keyborad=True)
+@router.message(Command(commands=['start']))
+async def start_function(message: Message):
+    await message.answer(
+        text='Добро пожаловать в бот!\n'
+        'Нажмите кнопку Начать для того, чтобы добавить аккаунт в требуемые чаты.\n',
+        reply_markup=make_row_keyboard(start_button)
+    )
 
 
-@router.message(Command('start_join'))
+@router.message(Text(text='Начать', ignore_case=True))
 async def set_api_id(message: Message, state: FSMContext):
     await message.answer(
         text='Введите api_id',
+        reply_markup=make_row_keyboard(return_button)
     )
     await state.set_state(Order.api_id)
+
+
+@router.message(Text(text='Назад', ignore_case=True))
+async def set_api_id(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer(
+        text='Вы вернулись на главную',
+        reply_markup=make_row_keyboard(start_button)
+    )
+
+
+@router.message(Text(text='Нет', ignore_case=True))
+async def set_api_id(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer(
+        text='Нажмите Начать, чтобы начать заново',
+        reply_markup=make_row_keyboard(start_button)
+    )
 
 
 @router.message(
@@ -36,7 +74,10 @@ async def set_api_id(message: Message, state: FSMContext):
 async def set_api_hash(message: Message, state: FSMContext):
     await state.update_data(api_id=message.text)
     print(message.text)
-    await message.answer(text='Далее введите api_hash')
+    await message.answer(
+        text='Далее введите api_hash',
+        reply_markup=make_row_keyboard(return_button)
+    )
     await state.set_state(Order.api_hash)
 
 
@@ -46,7 +87,10 @@ async def set_api_hash(message: Message, state: FSMContext):
 async def set_api_hash(message: Message, state: FSMContext):
     await state.update_data(api_hash=message.text)
     print(message.text)
-    await message.answer(text='Далее введите номер телефона')
+    await message.answer(
+        text='Далее введите номер телефона',
+        reply_markup=make_row_keyboard(return_button)
+    )
     await state.set_state(Order.number)
 
 
@@ -60,16 +104,69 @@ async def set_api_hash(message: Message, state: FSMContext):
         text=f'Введены следующие данные:\n'
              f'API_ID: {user_data["api_id"]}\n'
              f'API_HASH: {user_data["api_hash"]}\n'
-             f'Номер телефона: {user_data["number"]}'
+             f'Номер телефона: {user_data["number"]}\n'
+             f'Все верно?',
+        reply_markup=make_row_keyboard(available_answers)
     )
     await state.set_state(Order.code)
 
 
 @router.message(
     Order.code,
+    F.text == 'Да'
 )
 async def set_api_hash(message: Message, state: FSMContext):
     user_data = await state.get_data()
     app = Client('bot', api_id=user_data['api_id'], api_hash=user_data['api_hash'])
-    app.send_code(user_data['number'])
-    app.sign_in()
+    await app.connect()
+    sent_code = await app.send_code(user_data['number'])
+    await state.update_data(code_hash=sent_code.phone_code_hash, app=app)
+    await message.answer(text='Введите код подтверждения')
+    await state.set_state(Order.auth)
+
+
+@router.message(
+    Order.auth
+)
+async def get_code(message: Message, state: FSMContext):
+    await state.update_data(code=message.text)
+    user_data = await state.get_data()
+    app = user_data['app']
+    code = message.text
+    try:
+        signed_in = await app.sign_in(
+            phone_number=user_data['number'],
+            phone_code_hash=user_data['code_hash'],
+            phone_code=code
+        )
+        if isinstance(signed_in, User):
+            print('successful')
+        await message.answer(
+            text='Введите ссылки групп, в которые нужно вступить.\n'
+                 'Каждую ссылку вводите в новой строке\n'
+                 'Все ссылки должны находиться в одном сообщении.'
+        )
+    except Exception as e:
+        print(e)
+    await state.set_state(Order.urls_list)
+
+
+@router.message(
+    Order.urls_list
+)
+async def set_url(message: Message, state: FSMContext):
+    user_data = await state.get_data()
+    urls = message.text
+    urls = urls.split('\n')
+    await message.answer(
+        text=f'Всего введено {len(urls)} ссылок\n'
+            'Начинаю вступать. Как только закончу, придет соответствующее сообщение.'
+    )
+    await join(urls, user_data['app'])
+    await message.answer(
+        text='Готово!',
+        reply_markup=make_row_keyboard(start_button)
+    )
+    await state.clear()
+
+
